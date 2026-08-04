a6e90199a9b4f8fd50b79538b099ca1e7953f29f
Angel Rada <angelgrv20@gmail.com>
2026-08-04 10:44:10 -0400

    instal classic gruvbox theme

---
+++ nvim/.config/nvim/plugin/colorschemes/gruvbox.lua
@@ -0,0 +1,15 @@
+local github = require("config.utils").github
+local highlights = require("config.utils").highlights
+
+vim.pack.add({ github("ellisonleao/gruvbox.nvim") })
+
+require("gruvbox").setup({
+	bold = true,
+	transparent_mode = true,
+	contrast = "hard",
+	overrides = highlights({}, {}, {
+		GitSignsCurrentLineBlame = { fg = "#918374" },
+		LspInlayHint = { fg = "#555049" },
+		ErrorMsg = { bg = "none", fg = "#FB4834" },
+	}),
+})
